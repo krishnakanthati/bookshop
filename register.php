b93a4c099c0a570c225e5aa25ac2b9afbdc33bee
krishnakanthati <krishnakanthati@outlook.com>
2021-01-01 22:58:40 +0530

    register.php

--- register.php
+++ register.php
@@ -1,3 +1,85 @@
+<?php
+    require_once "config.php";
+
+    $username = $email = $password = $name = "";
+    $username_err = $email_err = $password_err = $name_err = "";
+
+    if ($_SERVER['REQUEST_METHOD'] == "POST") {
+        
+        // check if username is empty
+        if (empty(trim($_POST["username"]))) {
+            $username_err = "Username cannot be blank.";
+        } else {
+            $sql = "SELECT id FROM users WHERE username = ?";
+            $stmt = mysqli_prepare($conn, $sql);
+
+            if ($stmt) {
+                mysqli_stmt_bind_param($stmt, "s", $param_username);
+
+                // set the value of param username
+                $param_username = trim($_POST["username"]);
+
+                // execute the satement
+                if (mysqli_stmt_execute($stmt)) {
+                    mysqli_stmt_store_result($stmt);
+                    if (mysqli_stmt_num_rows($stmt) == 1) {
+                        $username_err = "This username is already taken.";
+                    } else {
+                        $username = trim($_POST["username"]);
+                    }
+                } else {
+                    echo "Something went wrong.";
+                }
+            }
+        }
+        mysqli_stmt_close($stmt);
+
+
+
+        // check for email
+        if (empty(trim($_POST["email"]))) {
+            $email_err = "Email cannot be blank.";
+        } else {
+            $email = trim($_POST["email"]);
+        }
+
+        // check for password
+        if (empty(trim($_POST["password"]))) {
+            $password_err = "Password cannot be blank.";
+        } elseif (strlen(trim($_POST["password"])) < 5) {
+            $password_err = "Password cannot be less than 5 characters";
+        } else {
+            $password = trim($_POST["password"]);
+        }
+
+        // if no errors, insert into database
+
+        if ( (empty($username_err)) && (empty($password_err)) && (empty($email_err)) && (empty($name_err)) ) {
+            $sql = "INSERT INTO users (username, password, email, name) VALUES (?, ?, ?, ?)";
+            $stmt = mysqli_prepare($conn, $sql);
+            if ($stmt) {
+                mysqli_stmt_bind_param($stmt, "ssss", $param_username, $param_password, $param_email, $param_name);
+
+                // set these parameters
+                $param_username = $username;
+                $param_password = password_hash($password, PASSWORD_DEFAULT);
+                $param_email = $email;
+                $param_name = $name;
+
+                // try to execute the query
+                if (mysqli_stmt_execute($stmt)) {
+                    header("location: login.php");
+                } else {
+                    echo "Something went wrong.";
+                }
+            }
+            mysqli_stmt_close($stmt);
+        }
+
+        mysqli_close($conn);
+    }
+?>
+
 <!doctype html>
 <html lang="en">
   <head>
@@ -62,11 +144,11 @@
             <div class="row">
                 <div class="form-group col-md-5">
                 <label for="inputEmail4">Email</label>
-                <input type="text" class="form-control" name="username" id="inputEmail4" placeholder="Email">
+                <input type="email" class="form-control" name="email" id="inputEmail4" placeholder="Email">
                 </div>
                 <div class="form-group col-md-5">
                 <label for="inputPassword4">Password</label>
-                <input type="password" class="form-control" name ="password" id="inputPassword4" placeholder="Password">
+                <input type="password" class="form-control" name="password" id="inputPassword4" placeholder="Password">
                 </div>
             </div>
             <br>
